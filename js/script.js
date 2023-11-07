058a0cbb76c80cb903d8c46627ce47c55e2373be
Mostafa <mostafa6320005.com>
2023-11-07 17:43:44 +0200

    upload files2

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const element = document.querySelector(".menu");
+const classToAdd = "style";
+const button = document.querySelector(".menu");
+button.addEventListener("click", () => {
+  if (element.classList.contains(classToAdd)) {
+    element.classList.remove(classToAdd);
+  } else {
+    element.classList.add(classToAdd);
+  }
+});
